c09521ce669c5a821a072427052a4c9f60063a6b
Nabil faturrahman <nabilfaturr44@gmail.com>
2024-07-17 15:15:47 +0700

    feat: setup drizzle

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,12 @@
-import { createClient } from "@libsql/client";
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 
-export const turso = createClient({
+config({ path: '.env' });
+
+const client = createClient({
   url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
+
+export const db = drizzle(client);
