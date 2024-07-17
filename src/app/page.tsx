439434036f35b9cbb0e276a1b9a9e079d55fca6f
Nabil faturrahman <nabilfaturr44@gmail.com>
2024-07-17 14:25:49 +0700

    feat: setup turso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <section>Home Page</section>
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
   );
 }
