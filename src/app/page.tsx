e9939564e11fd8b5ddc9788a4b937a0c990d998d
Nabil faturrahman <nabilfaturr44@gmail.com>
2024-07-17 15:32:08 +0700

    feat: setup auth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { auth, signIn, signOut } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <form
+        action={async () => {
+          "use server";
+
+          await signIn("google");
+        }}
+      >
+        <Button type="submit">Login</Button>
+      </form>
+      {session && (
+        <form
+          action={async () => {
+            "use server";
+
+            await signOut();
+          }}
+        >
+          <Button type="submit">Logout</Button>
+        </form>
+      )}
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </main>
   );
 }
