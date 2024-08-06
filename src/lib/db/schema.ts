d0c5cface8bcbd5e1f4d9a0452fcbde83bbf51ac
Nabil Faturrahman <nabilfaturr44@gmail.com>
2024-08-06 10:47:12 +0700

    feat: add class table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -5,8 +5,9 @@ import {
   primaryKey,
 } from "drizzle-orm/sqlite-core";
 import type { AdapterAccountType } from "next-auth/adapters";
+import {nanoid} from "nanoid"
 
-export const teachers = sqliteTable("teacher", {
+export const teachersTable = sqliteTable("teacher", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
@@ -17,7 +18,7 @@ export const teachers = sqliteTable("teacher", {
   image: text("image"),
 });
 
-export const students = sqliteTable("student", {
+export const studentsTable = sqliteTable("student", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
@@ -27,12 +28,29 @@ export const students = sqliteTable("student", {
   image: text("image"),
 });
 
-export const accounts = sqliteTable(
+export const classTable = sqliteTable("class", {
+  id: text("id").primaryKey().$defaultFn(() => crypto.randomUUID()),
+  classCode: text("classCode").notNull().$defaultFn(() => nanoid(6)),
+  name: text("name").notNull(),
+  subject: text("subject").notNull(),
+  level: text("level").notNull(),
+  description: text("description"),
+});
+
+export const studentClassTable = sqliteTable('student_class', {
+    studentId: text('student_id').notNull().references(() => studentsTable.id, { onDelete: 'cascade' }),
+    classId: text('class_id').notNull().references(() => classTable.id, { onDelete: 'cascade' }),
+    // using a compound key to join the two tables
+  }, (table) => ({
+    pk: primaryKey({columns: [table.studentId, table.classId]}) // pk is a compound key
+  }));
+
+export const accountsTable = sqliteTable(
   "account",
   {
     userId: text("userId")
       .notNull()
-      .references(() => students.id, { onDelete: "cascade" }),
+      .references(() => studentsTable.id, { onDelete: "cascade" }),
     type: text("type").$type<AdapterAccountType>().notNull(),
     provider: text("provider").notNull(),
     providerAccountId: text("providerAccountId").notNull(),
